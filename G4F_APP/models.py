e49d3aee7e417f981831a124280327522c739191
Luke Hopkins <2447720h@student.gla.ac.uk>
2021-03-29 18:01:36 +0100

    model setup

--- G4F_APP/models.py
+++ G4F_APP/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+	name = models.CharField(max_length=128, unique=True)
+	
+	def __str__(self):
+		return self.name
+
+	class Meta:
+		verbose_name_plural = "Categories"
+
+class Place(models.Model):
+
+
+	def __str__(self):
+		return self.title
+
+class Review(models.Model):
+
+
+	def __str__(self):
+		return self.title
+
+
+class UserProfile(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	userType = models.CharField(max_length=128)
+	dateJoined = models.DateField()
+	picture = models.ImageField(upload_to='profile_images', blank=True)
+
+	def __str__(self):
+		return self.user.username
