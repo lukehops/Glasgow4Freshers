2735689d98e86c53b6ec8a9617e7bfd98514b8b9
“Lewis <2397246L@student.gla.ac.uk>
2021-04-04 15:48:16 +0100

    fixed urls/logout option only displays when logged in

--- Glasgow4Freshers/urls.py
+++ Glasgow4Freshers/urls.py
@@ -18,8 +18,9 @@ from django.urls import path
 from G4F_APP import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import url
 
-
+app_name = 'G4F_APP'
 urlpatterns = [
 	path('', views.index, name="index"),
     path('admin/', admin.site.urls),
